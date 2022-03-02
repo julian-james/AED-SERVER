43747988035822ffeeaaed9250faea300707dd3b
Julian James <julian@Julians-MacBook-Pro.local>
2022-03-02 22:10:26 +0000

    static_root

--- app/aednearme/settings.py
+++ app/aednearme/settings.py
@@ -188,8 +188,8 @@ REST_FRAMEWORK = {
     )
 }
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
